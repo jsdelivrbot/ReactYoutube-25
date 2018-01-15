6564a1b955b37b38d0de936a0ff0f8b8c83257a1
rabiee3 <rabiee3.mohamed@gmail.com>
2018-01-15 21:45:03 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+
+const API_KEY = 'AIzaSyDE5n-vkX3UMwxXIepyidDGhZOxdNgYTlI';
+
+const App = function(){
+    return(
+    <div>hi</div>
+    );
+    
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
